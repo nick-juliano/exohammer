da7d5faf0949481ac52d5ac26119f762b7a1ba51
Nick Juliano <56493689+nick-juliano@users.noreply.github.com>
2025-07-03 12:15:36 -0400

    add docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,9 +5,11 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# conf.py
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../..'))
+
 
 project = 'exohammer'
 copyright = '2025, Nick Juliano'
